73d831c5c6cffb76b4e4fd1a1c2aa9f6f1b1c0ec
Gunnar R <gunnarrosenberg@gmail.com>
2021-07-19 23:00:16 -0700

    add license

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Keywords.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "keywords",
+      source_url: "https://github.com/GunnarPDX/keyword_parser"
     ]
   end
 
@@ -24,6 +28,21 @@ defmodule Keywords.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    """
+    Parses keywords from strings.
+    """
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/GunnarPDX/keyword_parser"}
     ]
   end
 end
