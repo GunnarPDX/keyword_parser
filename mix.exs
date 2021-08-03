ae11ed3a9cfdde8f64dcf8e35d095b7fdea5ad44
Gunnar R <gunnarrosenberg@gmail.com>
2021-08-02 22:32:06 -0700

    rustler

--- mix.exs
+++ mix.exs
@@ -26,7 +26,8 @@ defmodule Keywords.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:rustler, "~> 0.22.0"}
     ]
   end
 
