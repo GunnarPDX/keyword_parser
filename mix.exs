05a8f6740d4250fc3e1d37125474957cdaae0903
Gunnar R <gunnarrosenberg@gmail.com>
2025-04-03 11:39:41 -0700

    elixir-1.16

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Keywords.MixProject do
   def project do
     [
       app: :keywords,
-      version: "1.3.0",
-      elixir: "~> 1.12",
+      version: "1.4.1",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
